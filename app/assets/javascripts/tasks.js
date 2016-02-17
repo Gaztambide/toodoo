af8564e051a307c00efab9025bab1dc9b834f95c
Gaztambide <francogaztambide@gmail.com>
2016-02-17 18:42:16 -0500

    changed index view to include name:

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -5,11 +5,9 @@ $(document).on("ready", ready);
 function ready() {
   $('#new_task').on('ajax:success', newTask);
 
-  function newTask() {
+  function newTask(event, data) {
    window.location.reload();
+   console.log(arguments);
+   $('body').append(data);
  }
-}
-
-function newTask() {
-  console.log(arguments);
 }
