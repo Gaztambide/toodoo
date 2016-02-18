344da607cfaba467556587544087d56da07b3de5
Gaztambide <francogaztambide@gmail.com>
2016-02-17 19:00:58 -0500

    render only loads tasks

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).on("ready", ready);
-
+$(document).on("ready page:load", ready);
+$('#tasks').append(data);
 function ready() {
   $('#new_task').on('ajax:success', newTask);
 
@@ -9,5 +9,7 @@ function ready() {
    window.location.reload();
    console.log(arguments);
    $('body').append(data);
+   $('#tasks').append(data);
+   $('#task_name').val('');
  }
 }
